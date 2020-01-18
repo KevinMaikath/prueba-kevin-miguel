fbc7b51e8355e0a4ba9e0a20e8af45e7fc29d9bf
miguelleonmarti <miguelleonmarti@gmail.com>
2020-01-18 13:31:45 +0000

    imageTagger function (Google Cloud Vision API)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,55 @@
-import * as functions from 'firebase-functions';
+// Google Cloud Functions
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Firebase
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+// Cloud Vision
+const vision = require("@google-cloud/vision");
+const visionClient = new vision.ImageAnnotatorClient();
+
+// Dedicated bucket for cloud function invocation
+const bucketName: string = "squaads-vision";
+const collectionPath: string = "photos";
+
+/**
+ * Once a new image has been detected, it will be processed and 
+ * its results (isDog, imageUrl and labels) stored in a database document
+ */
+export const imageTagger = functions.storage
+
+  .bucket(bucketName)
+  .object()
+  .onFinalize(async object => {
+    // File data
+    const filePath: string = object.name || "";
+
+    // Location of saved file in bucket
+    const imageUri: string = `gs://${bucketName}/${filePath}`;
+
+    // Doc reference to save image processing results
+    const docRef = admin
+      .firestore()
+      .collection(collectionPath)
+      .doc();
+
+    // Await the cloud vision response
+    const results = await visionClient.labelDetection(imageUri);
+
+    // Map the data to desired format
+    const labels = results[0].labelAnnotations.map(
+      (obj: { description: any }) => obj.description
+    );
+
+    // Checking if the 'Dog' tag exists in labels
+    const isDog = labels.includes("Dog");
+
+    // Getting image url
+    const metadata: any = object.metadata || "";
+    const token: string = metadata.firebaseStorageDownloadTokens;
+    const imageUrl: string = `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${filePath}?alt=media&token=${token}`;
+
+    return docRef.set({ isDog, imageUrl, labels });
+  });
