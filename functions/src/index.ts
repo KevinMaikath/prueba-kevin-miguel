4bd78bce59eda112d7cf7df8e76714bb073586b7
miguelleonmarti <miguelleonmarti@gmail.com>
2020-01-19 12:44:23 +0000

    Refactoring

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,8 +11,6 @@ const app = new Clarifai.App({
 
 // Firebase
 import * as admin from "firebase-admin";
-import { DocumentReference } from "@google-cloud/firestore";
-
 admin.initializeApp(functions.config().firebase);
 
 // Cloud Vision
@@ -22,7 +20,6 @@ const visionClient = new vision.ImageAnnotatorClient();
 // Dedicated bucket for cloud function invocation
 const bucketName: string = "squaads-vision";
 const collectionPath: string = "photos";
-const collectionPathClarifai: string = "photosClarifai";
 
 /**
  * Once a new image has been detected, it will be processed (with Google Cloud Vision API)
@@ -51,36 +48,12 @@ export const imageTagger = functions.storage
     const results = await visionClient.labelDetection(imageUri);
 
     // Map the data to desired format
-    const onlyNameAndValue = (o: any) => {
-      return { name: o.description, value: o.score };
-    };
-    const labels = results[0].labelAnnotations.map(onlyNameAndValue);
+    const labelsGoogle = results[0].labelAnnotations.map(
+      onlyNameAndValueGoogle
+    );
 
     // Checking if the 'Dog' tag exists in labels
-    const isDog = isLabel(labels, "dog");
-
-    return docRef.set({ isDog, imageUrl, labels });
-  });
-
-/**
- * Once a new image has been detected, it will be processed (with Clarifai API)
- * and its results (isDog, imageUrl and labels) stored in a database document
- */
-export const imageTaggerClarifai = functions.storage
-  .bucket(bucketName)
-  .object()
-  .onFinalize(async object => {
-    // File data
-    const filePath: string = object.name || "";
-
-    // Doc reference to save image processing results
-    const docRef: DocumentReference = admin
-      .firestore()
-      .collection(collectionPathClarifai)
-      .doc();
-
-    // Getting image url
-    const imageUrl: string = getImageUrl(filePath, object);
+    const isDogGoogle = isLabel(labelsGoogle, "dog");
 
     // Await the Clarifai response
     app.models
@@ -93,13 +66,16 @@ export const imageTaggerClarifai = functions.storage
       })
       .then((response: any) => {
         const concepts = response["outputs"][0]["data"]["concepts"];
-        const onlyNameAndValue = (c: any) => {
-          return { name: c.name, value: c.value };
-        };
-        const labels = concepts.map(onlyNameAndValue);
-        const isDog = isLabel(labels, "dog");
+        const labelsClarifai = concepts.map(onlyNameAndValueClarifai);
+        const isDogClarifai = isLabel(labelsClarifai, "dog");
 
-        return docRef.set({ isDog, imageUrl, labels });
+        return docRef.set({
+          imageUrl,
+          isDogClarifai,
+          isDogGoogle,
+          labelsClarifai,
+          labelsGoogle
+        });
       });
   });
 
@@ -128,4 +104,20 @@ const isLabel = (labels: any[], name: string): boolean => {
       label.name.toLowerCase() === name;
     }) !== -1
   );
+};
+
+/**
+ * Returns name and value of each label
+ * @param l Label Annotation
+ */
+const onlyNameAndValueGoogle = (l: any) => {
+  return { name: l.description, value: l.score };
+};
+
+/**
+ * Returns name and value of each label
+ * @param c Concept
+ */
+const onlyNameAndValueClarifai = (c: any) => {
+  return { name: c.name, value: c.value };
 };
