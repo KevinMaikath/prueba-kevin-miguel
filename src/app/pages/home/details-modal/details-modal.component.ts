9331e4e01654ea68bfc234aec33c0bca9a41c2d6
Kevin <kevinrx25@gmail.com>
2020-01-19 13:22:52 +0000

    Details screen layout

--- src/app/pages/home/details-modal/details-modal.component.ts
+++ src/app/pages/home/details-modal/details-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ImagePreview} from '../../../models/image-preview';
+import {ModalController} from '@ionic/angular';
 
 @Component({
   selector: 'app-details-modal',
@@ -9,11 +10,20 @@ import {ImagePreview} from '../../../models/image-preview';
 export class DetailsModalComponent implements OnInit {
 
   @Input() image: ImagePreview;
+  selectedSegment: string;
 
-  constructor() {
+  constructor(private modalCtrl: ModalController) {
   }
 
   ngOnInit() {
+    this.selectedSegment = 'Google';
   }
 
+  onBackPressed() {
+    this.modalCtrl.dismiss();
+  }
+
+  segmentChanged(event) {
+    this.selectedSegment = event.detail.value;
+  }
 }
